49fcaf7670dc4deeb391f6be221337713ae09cb5
Roberto Braile <rbraile@k-lagan.com>
2019-07-25 20:03:15 +0200

    add img fixes

--- component/HomeContent/index.js
+++ component/HomeContent/index.js
@@ -5,14 +5,24 @@ const HomePage = () => {
   return (
     <section className='section'>
       <figure className='central-img-desktop'>
-        <img src='/static/img/iphones-final.png' alt='Iphones' />
+        <img
+          srcSet='/static/img/iphones-final@2x.png 2x,
+          /static/img/iphones-final@3x.png 3x'
+          src='/static/img/iphones-final.png'
+          alt='Iphones'
+        />
       </figure>
       <div className='content'>
         <figure>
           <img className='logo' src='/static/img/contoso.svg' />
         </figure>
         <figure className='central-img-mobile'>
-          <img src='/static/img/phones-mobile.png' alt='Iphones' />
+          <img
+            srcSet='/static/img/phones-mobile@2x.png 2x,
+            /static/img/phones-mobile@3x.png 3x'
+            src='/static/img/phones-mobile.png'
+            alt='Iphones'
+          />
         </figure>
         <article className='descent-primary'>
           <h1>Curabitur lobortis d lorem id bibendum ut id consectetur.</h1>
